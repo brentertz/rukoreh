1b6ef188a13f184c4c3ca3c52448f84c1e1ec9c5
Brent <brent.ertz@gmail.com>
2011-02-13 14:36:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rukoreh::Application.config.secret_token = 'c23c12bc7e32afb97c88dbe80820ca7fc12243628d121b7a6bc3d518d6f07de3f28f1e2076d04332d02e0163449dc28b956b5a0e4417eafa131dd51f755d387b'
